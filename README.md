ba38682cb1c0ac16542b8970ec887419302a7702
Juraj Szasz <juraj.szasz3@gmail.com>
2017-01-11 16:27:17 +0100

    Refractor code structure
    Add documentation

--- README.md
+++ README.md
@@ -1,8 +1,94 @@
 # csv2latex
 
-A simple Python script that converts CSV data into LaTeX table and sends
-the output to STDOUT.  
+A simple yet ingenious script that converts CSV data into LaTeX table and sends
+the output to STDOUT.  Optionally the output can be written into the file
+instead.
 
+
+## Installation
+
+The `csv2latex.py` script requires `unicodecsv` and `tabulate` Python packages
+to run.  The script itself is usually placed into the working directory,
+although it could be useful to place it e.g. into `~/bin` directory and add
+`~/bin` to `PATH`.
+
+Following subsections describe how to install the dependency packages.
+
+
+### Fedora
+
+    $ sudo dnf install -y python-unicodecsv python-tabulate
+
+
+### CentOS/RHEL
+
+    $ sudo pip install unicodecsv tabulate
+
+
+### Ubuntu/Debian
+
+    $ sudo apt-get install python-unicodecsv python-tabulate
+
+
+### Other Linux distro, Apple macOS, Other UNIX-like OS
+
+    $ sudo pip install pyyaml unicodecsv tabulate
+
+
+### MS Windows
+
+    > python -m pip install -U pip setuptools
+    > python -m pip install unicodecsv tabulate
+
+
+## Synopsis
+
+```
+usage: csv2latex.py [-h] [-v] [-o FILE] FILE
+
+Converts FILE (in CSV format) into LaTeX table and send the output to
+STDOUT. Optionally, if -o argument is specified, the output is written into the
+file instead.
+
+positional arguments:
+  FILE           input file
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --version  show program's version number and exit
+  -o FILE        write output to FILE
+```
+
+
+## Usage examples
+
+1. Convert input CSV data into LaTeX table and print output to `STDIN`
+
+        $ ./csv2latex.py data.csv
+
+2. Convert input CSV data into LaTeX table and redirect output into file:
+
+        $ ./csv2latex.py data.csv > table.tex
+
+3. You can also specify output file via argument:
+
+        $ ./csv2latex.py -o table.tex data.csv
+
+4. Convert input CSV data into LaTeX table and add output to file:
+
+        $ ./csv2latex.py data.csv >> tables.tex
+
+5. Making `csv2latex.py` globally accessible:
+
+        $ cp csv2latex.py ~/bin
+        $ export PATH=$PATH:~/bin
+        $ cd ~/data
+        $ ls
+        data.csv
+        $ csv2latex.py -o table.tex data.csv
+
+
+## License
 
 csv2latex.py -- Converts CSV data to LaTeX table.
 
--- csv2latex.py
+++ csv2latex.py
@@ -1,25 +1,88 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+#
+# csv2latex.py -- Converts CSV data to LaTeX table.
+#
+# Copyright (C) 2017  Juraj Szász <juraj.szasz3@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Converts CSV data to LaTeX table.
+
+usage: csv2latex.py [-h] [-v] [-o FILE] FILE
+
+Converts FILE (in CSV format) into LaTeX table and send the output to
+STDOUT. Optionally, if -o argument is specified, the output is written into the
+file instead.
+
+positional arguments:
+  FILE           input file
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --version  show program's version number and exit
+  -o FILE        write output to FILE
+
+"""
 
 from __future__ import unicode_literals
+import argparse
+import unicodecsv
 import sys
+from tabulate import tabulate
+
+__version__ = "0.1"
+
+
+# Set default encoding to UTF-8
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-from unicodecsv import reader
-from tabulate import tabulate
-from sys import argv
 
-if len(argv) > 1:
-    input_file = argv[1]
-else:
-    print "csv2latex need input file as argument"
+def main():
+    """Run initial code when this module is executed as a script.
 
-output_file=input_file.rsplit('.')[0] + ".tex"
-input=open(input_file)
-output=open(output_file, 'w')
+    1. Parse command line arguments using argparse.ArgumentParser object.
+    2. Load input CSV file using unicodecsv.reader()
+    3. Convert CSV data into LaTeX table using tabulate.tabulate()
+    4. Write output into the output file or print output to STDOUT.
 
-table=reader(input,encoding='utf-8-sig')
+    """
+    description = "Converts FILE (in CSV format) into LaTeX table and send " + \
+                  "the output to STDOUT.  Optionally, if -o argument is " + \
+                  "specified, the output is written into the file instead."
+    arg_parser = argparse.ArgumentParser(description=description)
+    arg_parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    arg_parser.add_argument("-o", metavar="FILE", dest='output_file', type=argparse.FileType('w'),
+                            help="write output to FILE")
+    arg_parser.add_argument("input_file", metavar="FILE", type=argparse.FileType('r'),
+                            help="input file")
+    args = arg_parser.parse_args()
 
-#output.write(tabulate(table,tablefmt='latex'))
-print tabulate(table,tablefmt='latex')
+    csv_reader = unicodecsv.reader(args.input_file, encoding='utf-8-sig')
+    output = tabulate(csv_reader, tablefmt='latex')
+
+    if args.output_file:
+        args.output_file.write(output)
+    else:
+        print output
+
+
+if __name__ == "__main__":
+    # This code is executed only when scientometry-data-proc is being run
+    # directly as a script.  Since local variables are allocated much faster
+    # than global variables, it is a good practice to encapsulate whole initial
+    # code into the main() function.
+    main()
